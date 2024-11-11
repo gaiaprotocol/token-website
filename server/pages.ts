cd6ec5b4966b494b2ae5eb0b4dfd46174327e27e
yjgaia <yjdev88@gmail.com>
2024-11-11 19:55:00 +0900

    Bump version to 0.0.15 in pages.ts and update MetaMaskSDKConnector for improved module imports

--- server/pages.ts
+++ server/pages.ts
@@ -11,7 +11,7 @@ UniversalEl.impl = el;
 html.impl = (htmlContent) => htmlContent;
 
 const GTAG_ID = "G-SD4BMR4ZKT";
-const VERSION = "0.0.14";
+const VERSION = "0.0.15";
 
 export function pages(
   path: string,
