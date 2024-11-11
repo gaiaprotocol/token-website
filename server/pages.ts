fe211da4aa80c0b66fadbb79cc4c3285db2854a1
yjgaia <yjdev88@gmail.com>
2024-11-11 16:13:55 +0900

    Bump version to 0.0.3 in pages.ts

--- server/pages.ts
+++ server/pages.ts
@@ -11,7 +11,7 @@ UniversalEl.impl = el;
 html.impl = (htmlContent) => htmlContent;
 
 const GTAG_ID = "G-SD4BMR4ZKT";
-const VERSION = "0.0.2";
+const VERSION = "0.0.3";
 
 export function pages(
   path: string,
