26a1407e56f9d76e7b5a0698ad0441ea16e8d0b6
yjgaia <yjdev88@gmail.com>
2024-11-11 19:40:57 +0900

    Bump version to 0.0.12 in pages.ts and update MetaMaskSDKConnector to handle mobile deep linking for MetaMask installation

--- server/pages.ts
+++ server/pages.ts
@@ -11,7 +11,7 @@ UniversalEl.impl = el;
 html.impl = (htmlContent) => htmlContent;
 
 const GTAG_ID = "G-SD4BMR4ZKT";
-const VERSION = "0.0.11";
+const VERSION = "0.0.12";
 
 export function pages(
   path: string,
